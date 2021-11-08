294084bc76f5aa315d0550ec0f14c3f592eb7784
YeikovS <alexgamexs@gmail.com>
2021-11-08 17:34:30 -0500

    Update AutoresController.java

--- src/main/java/com/examen/examen/controller/AutoresController.java
+++ src/main/java/com/examen/examen/controller/AutoresController.java
@@ -1,5 +1,101 @@
 package com.examen.examen.controller;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examen.examen.entity.*;
+import com.examen.examen.service.*;
+
+@RestController
+@RequestMapping("/api/libros")
 public class AutoresController {
 
+	@Autowired
+	private SAutores autoresService;
+
+	@GetMapping("/")
+	public String get() {
+		return "TODO OK";
+	}
+	
+	// CREAR
+	@PostMapping("/create")
+	public ResponseEntity<Autores> save(@RequestBody Autores autor) {
+		try {
+			Autores a = autoresService.create(autor);
+			return new ResponseEntity<>(a, HttpStatus.CREATED);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	// LISTAR
+	@GetMapping("/listar")
+	public ResponseEntity<List<Autores>> list() {
+		try {
+			List<Autores> list = new ArrayList<>();
+			list = autoresService.readAll();
+			if (list.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+			return new ResponseEntity<>(list, HttpStatus.OK);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	// BUSCAR {ID}
+	@GetMapping("/read/{id}")
+	public ResponseEntity<Autores> search(@PathVariable("id") long id) {
+		Autores autor = autoresService.read(id);
+		if (autor.getIdautor() > 0) {
+			return new ResponseEntity<>(autor, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	// ELIMINAR
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<HttpStatus> delete(@PathVariable("id") long id) {
+		try {
+			autoresService.delete(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	// ACTUALIZAR {ID}
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Autores> update(@RequestBody Autores a, @PathVariable("id") long id) {
+		try {
+			Autores autor = autoresService.read(id);
+			if (autor.getIdautor() > 0) {
+				autor.setNombres(a.getNombres());
+				autor.setApellidos(a.getApellidos());
+
+				return new ResponseEntity<>(autoresService.create(a), HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
 }
